3f7e5adcc63162e90143a3ce258850ca51329f15
Azazagich <qwertyu2289@ukr.net>
2024-08-20 10:11:26 +0300

    add: enum RoleName

---
+++ src/main/java/org/example/domain/RoleName.java
@@ -0,0 +1,10 @@
+package org.example.domain;
+
+/**
+ * The RoleName enum represents the roles available in the system.
+ * Each role has a name associated with it.
+ */
+public enum RoleName {
+    ADMINISTRATOR,
+    USER;
+}
